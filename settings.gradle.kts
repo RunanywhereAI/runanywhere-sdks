82bb35f6bbf955310206b77db1a4b1b9157b7e5d
Siddhesh2377 <siddheshsonar2377@gmail.com>
2026-01-22 11:04:03 +0530

    Fix :: - Fix Not Building in Android Studio
    Changes ::
    - Added Changes For the Project to work
    - Edited : settings.gradle.kts
    - Edited : gradle.properties
    
    Added ::
    - Added a New Doc BUILDING-PROJECR.MD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,8 +25,8 @@ dependencyResolutionManagement {
 rootProject.name = "RunAnywhere-Android"
 
 // Include SDK modules
-include(":sdk:runanywhere-kotlin")
-include(":sdk:runanywhere-kotlin:jni")
+include(":runanywhere-kotlin")
+include(":runanywhere-kotlin:jni")
 
 // Include example apps as composite builds to keep them self-contained
 includeBuild("examples/android/RunAnywhereAI")
